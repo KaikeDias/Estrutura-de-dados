d2859b9ff156e3d6c9e877b5d894c8b139d2d24b
Kaike Dias <kaikedias98@gmail.com>
2022-11-10 14:16:23 -0300

    Ordenação e busca

---
+++ ordenacao-e-busca/q8-2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+void insere(char elemento, char vetor[], int numElementos){
+    while(numElementos > 0 && elemento < vetor[numElementos - 1]){
+        vetor[numElementos] = vetor[numElementos-1];
+        numElementos--;
+    }
+
+    vetor[numElementos] = elemento; 
+}
+
+void insertionSort(char vetor[], int numElementos){
+    for(int i = 0; i < numElementos; i++){
+        insere(vetor[i], vetor, i);
+    }
+}
+
+int main(){
+    char vetor[] = {'c', 'f', 'a', 'g', 'w', 'b'};
+    int numElementos = sizeof(vetor) / sizeof(char);
+
+    insertionSort(vetor, numElementos);
+
+    for(int i = 0; i < numElementos; i++){
+        cout << vetor[i] << endl;
+    }
+
+    return 0;
+}
